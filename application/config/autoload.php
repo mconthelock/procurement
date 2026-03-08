eaed919bab44cc4ce17af9a347696455a2025d70
onthelock <s.chalormsak@gmail.com>
2026-03-08 16:59:07 +0700

    feat: Update autoload libraries, fix export in index.js, and add vendor feature documentation

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('env', 'handler', 'session');
+$autoload['libraries'] = array('env', 'handler');
 
 /*
 | -------------------------------------------------------------------
---
+++ note/vendor.md
@@ -0,0 +1,5 @@
+# Feature List
+
+Here's a sentence with a footnote. [^1]
+
+[^1]: This is the footnote.
